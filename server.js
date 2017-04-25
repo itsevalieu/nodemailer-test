3df7ceeccf02548edd3ea36eae23125ac15be276
Eva Lieu <itsevalieu@gmail.com>
2017-04-24 23:34:25 -0700

    Move middleware to server.js from mail.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var nodemailer = require("nodemailer");
+
 
 // Create Instance of Express
 // ======================================
@@ -20,6 +22,36 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // ======================================
 var mail= require("./mail.js");
 app.use("/mail", mail);
+
+var smtpTransport = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: "itsevalieu@gmail.com",
+		pass: "codemore"
+	},
+	tls: {rejectUnauthorized: false},
+	debug: true
+});
+
+var mailOptions = {
+	from: "'ievalieu@gmail.com' <ievalieu@gmail.com>",
+	to: "itsevalieu@gmail.com",
+	subject: "Test Email",
+	text: "Test Email"
+};
+
+console.log(mailOptions);
+
+smtpTransport.sendMail(mailOptions, function(error, response) {
+	if(error) {
+		console.log(error);
+		res.end("error");
+	} else {
+		console.log("Message sent: " + response.message);
+		res.end("sent");
+	}
+});
+
 
 // Listener
 // ======================================
